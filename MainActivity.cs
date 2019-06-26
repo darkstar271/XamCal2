6b64ec37749fc0cc70d1e53129ba6c1f143f9d24
Darren Gorman <saintred-tech@hotmail.com>
2019-06-26 14:31:17 +1200

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace XamCal2
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private TextView textMessage;
+        private TextView txtNum1, txtNum2;
+        private double Num1, Num2, Result;
+        private Button btnPlus, btnMinus, btnMul, btndivide, btnThatButton;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            // this is like a constructor 
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+
+            btnMinus = FindViewById<Button>(Resource.Id.btnminus);
+
+
+
+
+
+
+
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
